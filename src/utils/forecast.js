226173d186ad03eef2e4bdf614c60e75bbe6d0a6
chroc <chroc7@gmail.com>
2022-04-21 16:08:45 +1000

    Remove comments

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,5 @@
 const request = require('postman-request');
 
-// request({ url: url, json: true }, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to the weather service :(');
-//     }else if(response.body.error){
-//         console.log('Unable to find location :(');
-//     }else{
-//         // convert the Json response
-//         // const data = JSON.parse(response.body); // since we add the json:true parameter
-//         // is no longer necesary to parse it to an object
-//         // console.log(response.body.current);
-//         const weatherDescription = response.body.current.weather_descriptions[0];
-//         const temperature = response.body.current.temperature;
-//         const feelsLike = response.body.current.feelslike;
-//         console.log(weatherDescription + ' .It is currently ' + temperature + ' degress out. It feels like ' + feelsLike + ' degress out.');
-//     }
-// });
-
 const forecast = (latitude, longitude, callback) => {
     //..
     const url = ('http://api.weatherstack.com/current?access_key=2e329d0dab328209a6b1bdbb4bc476e2&query='+ latitude +','+ longitude);
