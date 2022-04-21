2c3b839e201f681913dc76dd7730d433e1b14190
chroc <chroc7@gmail.com>
2022-04-21 16:24:49 +1000

    Add new Forecast data, Humidity and UV index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,6 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    //..
     const url = ('http://api.weatherstack.com/current?access_key=2e329d0dab328209a6b1bdbb4bc476e2&query='+ latitude +','+ longitude);
     request({ url, json: true }, (error, { body }) => {
         if(error){
@@ -12,7 +11,10 @@ const forecast = (latitude, longitude, callback) => {
             const weatherDescription = body.current.weather_descriptions[0];
             const temperature = body.current.temperature;
             const feelsLike = body.current.feelslike;
-            const message = weatherDescription + ' .It is currently ' + temperature + ' degress out. It feels like ' + feelsLike + ' degress out.';
+            const humidity = body.current.humidity;
+            const uvIndex = body.current.uv_index;
+            console.log(body.current);
+            const message = weatherDescription + ' .It is currently ' + temperature + '°C out. It feels like ' + feelsLike + '°C out. Humidity: ' + humidity + '%. UV index: ' + uvIndex;
             callback(undefined, message);
         }
     });
